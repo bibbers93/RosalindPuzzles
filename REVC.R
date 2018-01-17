75f117cd5cfc2aa24786eeb011a22978383e103d
Lloyd <32960596+bibbers93@users.noreply.github.com>
2018-01-17 21:10:56 +0000

    Update REVC.R

--- REVC.R
+++ REVC.R
@@ -1,11 +1,9 @@
 # Take sequence string and reverse order
-
 a <- "AGCTA"
 a1 <- strsplit(a, split="") [[1]]
 a2 <- rev(a1)
 
 # Take reversed string and complement the sequence with the following loop
-
 a3 <- character(length=0)
 for(i in 1:length(a2)){
   if(a2[i]=="A"){
@@ -23,5 +21,4 @@ for(i in 1:length(a2)){
 }
 
 # Recreate sequence string
-
 a4 <- paste(a3, collapse = "")
